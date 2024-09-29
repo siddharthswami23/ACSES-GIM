b933549594dfa02f75865f965c33bcd5fdc98168
siddharthswami23 <siddharth.swami@walchandsangli.ac.in>
2024-09-29 15:25:04 +0530

    commit

--- leaderboard/leaderboard.js
+++ leaderboard/leaderboard.js
@@ -7,11 +7,11 @@ window.onload = function () {
 
             data.forEach((entry, index) => {
                 const box = document.createElement('div');
-                box.classList.add('w-[500px]', 'mt-5', 'ml-auto', 'mr-auto', 'py-5', 'px-3', 'rounded-sm', 'bg-slate-500', 'bg-opacity-80');
+                box.classList.add('w-[500px]', 'mt-5', 'ml-auto', 'mr-auto', 'py-5', 'px-3', 'rounded-sm', 'bg-blue-200', 'bg-opacity-80');
                 
                 box.innerHTML = `
                 <div
-        class="box flex justify-between items-center text-white text-2xl font-bold font-[cursive] border-2 border-black px-3 py-5 m-2"
+        class="box flex justify-between items-center text-white text-2xl font-bold font-[cursive] border-2 border-black px-3 py-5"
       >
         <h1>${index + 1}. ${entry.username}</h1>
         <h1>${entry.points}</h1>
